7d906fb9687ec5c00a1d90fe5bed35db9ff4eca5
warlockja <warlockja@gmail.com>
2023-06-30 10:23:10 +0300

    added url parameter dark mode switch

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,13 @@ import Error from "./Components/Error";
 import FreeCurrencyAPI from "./Components/FreeCurrencyAPI";
 import Loading from "./Components/Loading";
 import useFetch from "./hooks/useFetch";
+import setTheme from "./util/setTheme";
 
 function App() {
+  const params = new URLSearchParams(window.location.search);
+  const theme = params.get("theme");
+  if (theme) setTheme(theme);
+
   const { isLoading, data, error } = useFetch(
     import.meta.env.VITE_APP_FREECURRENCYAPI_URL
   );
